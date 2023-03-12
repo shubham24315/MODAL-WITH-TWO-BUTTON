a6412d0ebc28d1d05e5b7e3d6b689c271b9e280e
shubham24315 <103456566+shubham24315@users.noreply.github.com>
2023-03-12 22:09:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Get the pop-up
+var popup = document.getElementById("myPopup");
+var btn=document.getElementsById("btn");
+// Function to open the pop-up
+var ok=document.getElementsById("ok");
+var close=document.getElementsById("close");
+var body=document.getElementsByName("body");
+function openPopup() {
+  popup.style.display = "block";
+   ok.addEventListener("click",function(){
+  
+   
+   });
+}
+
+// Function to close the pop-up
+function closePopup() {
+  popup.style.display = "none";
+  
+}
